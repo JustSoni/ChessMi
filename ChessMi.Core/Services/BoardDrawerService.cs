a7417c8b7e5559b540f50fc4d2498ecb961a8021
Soner Kochan <78814531+JustSoni@users.noreply.github.com>
2022-10-29 12:27:22 +0300

    Added: BoardDrawerService which will be used to print out the board on the console. To be finished.

---
+++ ChessMi.Core/Services/BoardDrawerService.cs
@@ -0,0 +1,16 @@
+﻿using ChessMi.Core.Data.Enums;
+using ChessMi.Core.Data.Models;
+using static ChessMi.Core.Common.GlobalConstants;
+
+namespace ChessMi.Core.Services
+{
+    public class BoardDrawerService
+    {
+        private static readonly string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
+
+        public static void DrawBoard(Tile[,] board)
+        {
+            
+        }
+    }
+}
