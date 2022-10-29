d8f9ec7c374eeda17b4beb86430c23edadf00dc2
Soner Kochan <78814531+JustSoni@users.noreply.github.com>
2022-10-29 12:37:39 +0300

    Added: Regex validator if the input format is correct.

--- ChessMi/Program.cs
+++ ChessMi/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using ChessMi.Core.Data.Models;
+using ChessMi.Core.Services;
+using System.Text.RegularExpressions;
+
+Tile[,] board = BoardGeneratorService.Generate();
+
+BoardDrawerService.DrawBoard(board);//TODO: Implement it.
+
+MoveService service = new MoveService();
+
+MoveInfo move = new MoveInfo(false);
+
+string moveInput = String.Empty;
+
+while (true)
+{
+    moveInput = Console.ReadLine();
+
+    if (!Regex.IsMatch(moveInput, "^[a-h][1-8][-][a-h][1-8]$"))
+    {
+        Console.WriteLine("Invalid input format! Try again.");
+        continue;
+    }
+
+    if (moveInput == "0")
+    {
+        break;
+    }
+}
