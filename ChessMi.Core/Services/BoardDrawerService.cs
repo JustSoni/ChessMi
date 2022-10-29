501a79485d979f2acc3d0c65f694b1a383ac7cdc
Soner Kochan <78814531+JustSoni@users.noreply.github.com>
2022-10-29 14:21:18 +0300

    Update: Finished BoardDrawerService. White figures are drawn green while black one are red.

--- ChessMi.Core/Services/BoardDrawerService.cs
+++ ChessMi.Core/Services/BoardDrawerService.cs
@@ -10,7 +10,101 @@ namespace ChessMi.Core.Services
 
         public static void DrawBoard(Tile[,] board)
         {
-            
+            int rowCount = 8;
+
+            const string top = " -----------------";
+            for (int i = 0; i < BoardRows; i++)
+            {
+                for (int k = 0; k < BoardColumns; k++)
+                {
+                    Console.Write("|      ");
+                }
+
+                Console.WriteLine("|");
+
+                for (int j = 0; j < BoardColumns; j++)
+                {
+
+                    if (board[i, j].Figure.Name == "Rook")
+                    {
+                        Console.Write("| ");
+                        if (board[i, j].Figure.Color == Color.White)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Rook ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (board[i, j].Figure.Name == "Knight")
+                    {
+                        Console.Write("|");
+                        if (board[i, j].Figure.Color == Color.White)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Knight");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (board[i, j].Figure.Name == "Bishop")
+                    {
+                        Console.Write("|");
+                        if (board[i, j].Figure.Color == Color.White)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Bishop");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (board[i, j].Figure.Name == "Queen")
+                    {
+                        Console.Write("| ");
+                        if (board[i, j].Figure.Color == Color.White)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Queen");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (board[i, j].Figure.Name == "King")
+                    {
+                        Console.Write("|");
+                        if (board[i, j].Figure.Color == Color.White)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(" King ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (board[i, j].Figure.Name == "Pawn")
+                    {
+                        Console.Write("|");
+                        if (board[i, j].Figure.Color == Color.White)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(" Pawn ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.Write("|      ");
+                    }
+                }
+                Console.Write($"| {rowCount--}");
+                Console.WriteLine();
+                for (int k = 0; k < BoardColumns; k++)
+                {
+                    Console.Write("|______");
+                }
+
+                Console.WriteLine("|");
+            }
+            Console.Write("   ");
+            for (int i = 0; i < BoardColumns; i++)
+            {
+                Console.Write($"{letters[i]}      ");
+            }
+            Console.WriteLine();
         }
     }
 }
