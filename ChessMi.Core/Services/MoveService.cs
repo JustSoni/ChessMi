fd9724a7a2ff7f5a6bb5904c4b0bdf63a9b0e66d
Soner Kochan <78814531+JustSoni@users.noreply.github.com>
2022-10-29 15:03:10 +0300

    Update: Added Knight moves.

--- ChessMi.Core/Services/MoveService.cs
+++ ChessMi.Core/Services/MoveService.cs
@@ -97,7 +97,53 @@ namespace ChessMi.Core.Services
 
         public MoveInfo CheckLegalMove(Tile[,] board, Knight knight, Figure endPoint)
         {
-            throw new NotImplementedException();
+            if (this.ColorMatches(knight, endPoint))
+            {
+                return new MoveInfo(false);
+            }
+
+            MoveInfo move = new MoveInfo(false);
+
+            if (IsEmpty(endPoint)) // This code will run if the knight won't take a figure.
+            {
+                //Vertical move
+                if (Math.Abs(knight.Row - endPoint.Row) == 2 &&
+                    Math.Abs(knight.Column - endPoint.Column) == 1)
+                {
+                    move.IsAllowed = true;
+                    return move;
+                }
+
+                //Horizontal move
+                if (Math.Abs(knight.Row - endPoint.Row) == 1 &&
+                    Math.Abs(knight.Column - endPoint.Column) == 2)
+                {
+                    move.IsAllowed = true;
+                    return move;
+                }
+            }
+            else // This code will run if the knight will take a figure.
+            {
+                //Vertical move
+                if (Math.Abs(knight.Row - endPoint.Row) == 2 &&
+                    Math.Abs(knight.Column - endPoint.Column) == 1)
+                {
+                    move.FigureTaken = true;
+                    move.IsAllowed = true;
+                    return move;
+                }
+
+                //Horizontal move
+                if (Math.Abs(knight.Row - endPoint.Row) == 1 &&
+                    Math.Abs(knight.Column - endPoint.Column) == 2)
+                {
+                    move.FigureTaken = true;
+                    move.IsAllowed = true;
+                    return move;
+                }
+            }
+
+            return new MoveInfo(false);
         }
 
         public MoveInfo CheckLegalMove(Tile[,] board, Bishop bishop, Figure endPoint)
