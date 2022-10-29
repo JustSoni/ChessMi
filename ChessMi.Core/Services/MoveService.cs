66c77dc59558b89b4e84560fa25b7dd2f5c9f278
Soner Kochan <sonibestbg@gmail.com>
2022-10-29 19:26:07 +0300

    Update: Bishop movement done.

--- ChessMi.Core/Services/MoveService.cs
+++ ChessMi.Core/Services/MoveService.cs
@@ -148,7 +148,85 @@ namespace ChessMi.Core.Services
 
         public MoveInfo CheckLegalMove(Tile[,] board, Bishop bishop, Figure endPoint)
         {
-            throw new NotImplementedException();
+            if (this.ColorMatches(bishop, endPoint))
+            {
+                return new MoveInfo(false);
+            }
+
+            MoveInfo move = new MoveInfo(false);
+
+            int deltaRow = bishop.Row - endPoint.Row;
+            int deltaColumn = bishop.Column - endPoint.Column;
+
+
+            if (Math.Abs(deltaRow) != Math.Abs(deltaColumn))
+            {
+                return new MoveInfo(false);
+            }
+
+            if (!IsEmpty(endPoint)) // No matter what if there is a figure at the given position it will take it only if the move is valid.
+            {
+                move.FigureTaken = true;
+            }
+
+            if (deltaRow > 0 && deltaColumn > 0) // UP-LEFT
+            {
+                for (int i = bishop.Row - 1, j = bishop.Column - 1; i != endPoint.Row; i--, j--)
+                {
+                    if (!IsEmpty(board[i, j].Figure))
+                    {
+                        return new MoveInfo(false);
+                    }
+                }
+
+                move.IsAllowed = true;
+                return move;
+            }
+
+            if (deltaRow > 0 && deltaColumn < 0) // UP-RIGHT
+            {
+                for (int i = bishop.Row - 1, j = bishop.Column + 1; i != endPoint.Row; i--, j++)
+                {
+                    if (!IsEmpty(board[i, j].Figure))
+                    {
+                        return new MoveInfo(false);
+                    }
+                }
+
+                move.IsAllowed = true;
+                return move;
+            }
+
+            if (deltaRow < 0 && deltaColumn > 0) // DOWN-LEFT
+            {
+                for (int i = bishop.Row + 1, j = bishop.Column - 1; i != endPoint.Row; i++, j--)
+                {
+                    if (!IsEmpty(board[i, j].Figure))
+                    {
+                        return new MoveInfo(false);
+                    }
+                }
+
+                move.IsAllowed = true;
+                return move;
+            }
+
+            if (deltaRow < 0 && deltaColumn < 0) // DOWN-RIGHT
+            {
+                for (int i = bishop.Row + 1, j = bishop.Column + 1; i != endPoint.Row; i++, j++)
+                {
+                    if (!IsEmpty(board[i, j].Figure))
+                    {
+                        return new MoveInfo(false);
+                    }
+                }
+
+                move.IsAllowed = true;
+                return move;
+            }
+
+
+            return new MoveInfo(false);
         }
 
         public MoveInfo CheckLegalMove(Tile[,] board, Rook rook, Figure endPoint)
